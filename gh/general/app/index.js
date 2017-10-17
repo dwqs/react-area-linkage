2896c5453ead3a37cece65cf9ccbe488ed5c2194
dwqs <461147874@qq.com>
2017-10-17 21:07:16 +0800

    add demo

---
+++ gh/general/app/index.js
@@ -0,0 +1,13 @@
+import React, {Component, Children} from 'react';
+
+export default class App extends Component {
+    constructor (){
+        super ();
+    }
+
+    render () {
+        return (
+            <div>app test</div>
+        );
+    }
+}
---
+++ gh/page/index.js
@@ -0,0 +1,34 @@
+import React, {Component, Children} from 'react';
+import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import App from '../general/app/index';
+
+const env = process.env.NODE_ENV || 'development';
+
+if(env === 'development'){
+    window.onload = function () {
+        const render = Component => {
+            ReactDOM.render(
+                <AppContainer>
+                    <Component />
+                </AppContainer>,
+                document.getElementById('app')
+            );
+        };
+
+        render(App);
+
+        // HMR
+        if (module.hot) {
+            module.hot.accept('../general/app/index', () => { render(App); });
+        }
+    };
+} else {
+    window.onload = function () {
+        ReactDOM.render(
+            App,
+            document.getElementById('app')
+        );
+    };
+}
