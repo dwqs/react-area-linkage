58132fb7808f46496ac2841aa7eef7c3f897cb8a
dwqs <461147874@qq.com>
2017-10-18 16:08:27 +0800

    Setup demo components

--- gh/general/app/index.js
+++ gh/general/app/index.js
@@ -1,4 +1,11 @@
+import './index.less';
+
 import React, {Component, Children} from 'react';
+
+import Header from '@components/header/index';
+import Start from '@components/start/index';
+import Main from '@components/main/index';
+import Footer from '@components/footer/index';
 
 export default class App extends Component {
     constructor (){
@@ -7,7 +14,12 @@ export default class App extends Component {
 
     render () {
         return (
-            <div>app test</div>
+            <div className="app-wrap">
+                <Header />  
+                <Start />
+                <Main />
+                <Footer />
+            </div>
         );
     }
 }
