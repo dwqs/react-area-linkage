256264d64ccfd332c63c9b34df5e190b78f83df5
dwqs <461147874@qq.com>
2017-10-19 18:37:11 +0800

    Init state & props

--- src/area-select.js
+++ src/area-select.js
@@ -12,19 +12,52 @@ const Option = Select.Option;
 export default class AreaSelect extends Component {
     constructor (props) {
         super(props);
+        this.state = {
+            provinces: AreaData['86'],
+            citys: {},
+            areas: {},
+            streets: {},
+            curProvince: '',
+            curCity: '',
+            curArea: '',
+            curStreet: '',
+            defaults: [],
+            isCode: false,
+            isSetDefault: false
+        };
     }
 
     static propTypes = {
-
+        type: PropTypes.string,
+        placeholders: PropTypes.array,
+        level: PropTypes.number,
+        size: PropTypes.string
     };
 
     static defaultProps = {
-
+        type: 'code',
+        placeholders: [],
+        level: 1,
+        size: 'medium'
     };
 
     render () {
+        let { size, type } = this.props;
+
+        if(!['small', 'medium', 'large'].includes(size)) {
+            size = 'medium';
+        }
+
+        if(!['all', 'code', 'text'].includes(type)) {
+            type = 'code';
+        }
+
+        let defaultClass = 'area-select';
+        let classes = size === 'medium' ? 'medium' : size === 'small' ? 'small' : 'large';
+        classes = defaultClass + ' ' +  defaultClass;
+
         return (
-            <div className="area-select">
+            <div className={classes}>
 
             </div>
         );
