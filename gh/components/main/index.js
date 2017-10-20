bf0db8f73739173651ec0c8e5a1a9ba0e3016d65
dwqs <461147874@qq.com>
2017-10-21 01:26:41 +0800

    Update

--- gh/components/main/index.js
+++ gh/components/main/index.js
@@ -4,6 +4,8 @@ import React, {Component, Children} from 'react';
 
 import Basic from './basic';
 import Size from './size';
+import AreaText from './area-text';
+import AreaTextCode from './area-text-code';
 
 export default class Main extends Component {
     constructor (){
@@ -18,7 +20,11 @@ export default class Main extends Component {
                 <h5>默认形式</h5>
                 <Basic />
                 <h5>改变大小</h5>
-                <Size></Size>
+                <Size />
+                <h5>返回区域文本</h5>
+                <AreaText />
+                <h5>返回区域代码和文本</h5>
+                <AreaTextCode />
             </div>
         );
     }
