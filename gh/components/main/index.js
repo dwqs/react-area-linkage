0b53c6217d4593a10312c1e4a6a0b647153b6d7c
dwqs <461147874@qq.com>
2017-10-21 02:57:31 +0800

    Update

--- gh/components/main/index.js
+++ gh/components/main/index.js
@@ -6,6 +6,7 @@ import Basic from './basic';
 import Size from './size';
 import AreaText from './area-text';
 import AreaTextCode from './area-text-code';
+import DefaultVal from './default-value';
 
 export default class Main extends Component {
     constructor (){
@@ -27,6 +28,23 @@ export default class Main extends Component {
                 <AreaTextCode />
                 <h5>设置 placeholders</h5>
                 <Basic placeholders={['选择省', '选择市']}/>
+                <p className='desc'>
+                    <code>placeholders</code>&nbsp;是一个数组, 数组项顺序分别对应省/市/区/街道.
+                </p>
+                <h5>设置默认值及省市区联动</h5>
+                <DefaultVal level={2} />
+                <p className='desc'>
+                    <code>selected</code>&nbsp;是一个数组, 数组项顺序分别对应省/市/区/街道, 且类型(区域代码/区域文本)必须统一. 以第一个元素类型为基准. 类型不统一将报错.
+                </p>
+                <p className='desc'>
+                    <code>level</code>&nbsp;表示联动层级, 0-->一联 1->二联 2->三联 3->四联.
+                </p>
+                <h5>省市区街联动</h5>
+                <DefaultVal level={3} type='text' />
+                <h5>只选省份</h5>
+                <DefaultVal level={0} type='all' />
+                <h4>2. 作为级联器(只支持2/3级联动)</h4>
+                <h5>默认形式</h5>
             </div>
         );
     }
