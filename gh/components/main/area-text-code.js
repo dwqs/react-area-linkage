6bcd1609dee40daf2f3057d0c2be55392a66afda
dwqs <461147874@qq.com>
2017-10-21 01:26:24 +0800

    Add return text and code demo

---
+++ gh/components/main/area-text-code.js
@@ -0,0 +1,47 @@
+import React, {Component, Children} from 'react';
+
+import { AreaSelect } from '../../../src/index';
+
+export default class AreaText extends Component {
+    constructor (){
+        super ();
+        this.state = {
+            selected: [],
+            shown: false
+        };
+    }
+
+    handleSelectedChange = (val) => {
+        this.setState({
+            selected: val
+        });
+    }
+
+    toggle = () => {
+        this.setState({
+            shown: !this.state.shown
+        });
+    }
+
+    render () {
+        return (
+            <div className="code-area">
+                <div className="area-left">
+                    <AreaSelect type='all' onChange={this.handleSelectedChange}/>
+                </div>
+                <div className="area-right">
+                    <pre><code>{`${JSON.stringify(this.state.selected)}`}</code></pre>
+                </div>
+                {
+                    this.state.shown &&
+                    <div className="original-code">
+                        <pre><code><span>//返回值是一个数组，数组项是包含省市的行政区域代码和文本的对象</span><br /><span>&lt;</span>AreaSelect&nbsp; type='all' onChange=callback/<span>&gt;</span></code></pre>
+                    </div>
+                }
+                <div className="show-code" onClick={this.toggle}>
+                    {this.state.shown ? 'Hide Code' : 'Show Code'}
+                </div>
+            </div>
+        );
+    }
+}
