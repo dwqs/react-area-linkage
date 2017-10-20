3f63dfcdd3ee4cc9c77568ab93922765b2ff2c69
dwqs <461147874@qq.com>
2017-10-21 00:41:55 +0800

    Add size demo

--- gh/components/main/index.js
+++ gh/components/main/index.js
@@ -3,6 +3,7 @@ import './index.less';
 import React, {Component, Children} from 'react';
 
 import Basic from './basic';
+import Size from './size';
 
 export default class Main extends Component {
     constructor (){
@@ -16,6 +17,8 @@ export default class Main extends Component {
                 <h4>1. 作为选择器</h4>
                 <h5>默认形式</h5>
                 <Basic />
+                <h5>改变大小</h5>
+                <Size></Size>
             </div>
         );
     }
---
+++ gh/components/main/size.js
@@ -0,0 +1,47 @@
+import React, {Component, Children} from 'react';
+
+import { AreaSelect } from '../../../src/index';
+
+export default class Size extends Component {
+    constructor (){
+        super ();
+        this.state = {
+            selected: [],
+            shown: false
+        };
+    }
+
+    handleSelectedChange = (val) => {
+        this.setState({
+            selected: val
+        });
+    }
+
+    toggle = () => {
+        this.setState({
+            shown: !this.state.shown
+        });
+    }
+
+    render () {
+        return (
+            <div className="code-area">
+                <div className="area-left">
+                    <AreaSelect size='large' onChange={this.handleSelectedChange}/>
+                </div>
+                <div className="area-right">
+                    <pre><code>{`[${this.state.selected}]`}</code></pre>
+                </div>
+                {
+                    this.state.shown &&
+                    <div className="original-code">
+                        <pre><code><span>//size:['small','default','large'], 默认值是 default</span><br /><span>&lt;</span>AreaSelect&nbsp;size='large' onChange=callback/<span>&gt;</span></code></pre>
+                    </div>
+                }
+                <div className="show-code" onClick={this.toggle}>
+                    {this.state.shown ? 'Hide Code' : 'Show Code'}
+                </div>
+            </div>
+        );
+    }
+}
