ec8db6edcf5ee90f1e063972d3fc8aa433eae6b6
dwqs <461147874@qq.com>
2017-10-21 16:21:27 +0800

    fix

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -37,7 +37,7 @@ prodConfig.module.rules.unshift({
 });
 
 prodConfig.plugins = (prodConfig.plugins || []).concat([
-    new CleanWebpackPlugin(['dmeo'], {
+    new CleanWebpackPlugin(['demo'], {
         root: path.join(__dirname, '../'),
         verbose: true,
         dry: false
