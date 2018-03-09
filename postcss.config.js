d7ef657ad52e769a78282d3aa9b93023b27dbd89
dwqs <461147874@qq.com>
2018-03-09 23:21:30 +0800

    Update

--- postcss.config.js
+++ postcss.config.js
@@ -6,8 +6,6 @@
 //使用 happypack 之后 需单独提供 postcss 配置文件
 module.exports = {
     plugins: [
-        require('autoprefixer')({ browsers: ['last 5 versions','Android >= 4.0', 'iOS >= 7'] }),
-        require('precss')(),
-        require('cssnano')({zindex: false})
+        require('autoprefixer')({ browsers: ['last 5 versions','Android >= 4.0', 'iOS >= 7'] })
     ]
 };
