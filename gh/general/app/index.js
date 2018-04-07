54663090f04874f1d895dc8771af583105a3e9c5
dwqs <461147874@qq.com>
2018-04-08 00:17:07 +0800

    refactor: 补充 demo

--- gh/general/app/index.js
+++ gh/general/app/index.js
@@ -7,17 +7,31 @@ import Start from '@components/start/index';
 import Main from '@components/main/index';
 import Footer from '@components/footer/index';
 
+import AreaSelect from '../../../components/area-select';
+
 export default class App extends Component {
     constructor (){
         super ();
     }
 
+    onChange = (val) => {
+        console.log('aaaaaa', val);
+    }
+
     render () {
         return (
             <div className="app-wrap">
+                <p>1</p>
+                <p>1</p>
+                <p>1</p>
+                <p>1</p>
+                <p>1</p>
+                <AreaSelect level={2} onChange={this.onChange}/>
                 <Header />  
                 <Start />
-                <Main />
+                {
+                    // <Main />
+                }
                 <Footer />
             </div>
         );
