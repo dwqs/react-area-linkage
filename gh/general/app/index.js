1c0b7f75d18667824e0e9cd8f61991bb2d2a2bdb
dwqs <461147874@qq.com>
2018-04-09 00:15:24 +0800

    refactor: 开发 demo

--- gh/general/app/index.js
+++ gh/general/app/index.js
@@ -7,11 +7,15 @@ import Start from '@components/start/index';
 import Main from '@components/main/index';
 import Footer from '@components/footer/index';
 
-import AreaSelect from '../../../components/area-select';
+import AreaSelect from '../../../components/area-select/index';
+import AreaCascader from '../../../components/area-cascader/index';
 
 export default class App extends Component {
     constructor (){
         super ();
+        this.state = {
+            selected: [] //['广东省', '深圳市', '南山区'] //['440000','440300','440305']
+        };
     }
 
     onChange = (val) => {
@@ -21,19 +25,28 @@ export default class App extends Component {
     render () {
         return (
             <div className="app-wrap">
-                <p>1</p>
-                <p>1</p>
-                <p>1</p>
-                <p>1</p>
-                <p>1</p>
-                <AreaSelect level={2} onChange={this.onChange}/>
+                {
+                    // <p>1</p>
+                    // <p>1</p>
+                    // <p>1</p>
+                    // <p>1</p>
+                    // <p>1</p>
+                    // <AreaCascader onChange={this.onChange} level={1}/>
+                    // <AreaSelect  level={1} onChange={this.onChange}/>
+                }
                 <Header />  
                 <Start />
-                {
-                    // <Main />
-                }
+                <Main />
                 <Footer />
             </div>
         );
+    }
+
+    componentDidMount () {
+        // setTimeout(() => {
+        //     this.setState({
+        //         selected: ['440000','440300','440305']
+        //     });
+        // }, 2000);
     }
 }
