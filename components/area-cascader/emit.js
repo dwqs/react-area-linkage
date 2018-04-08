a35b6b89e95ef1a0256b88cd3ae7541b065fce66
dwqs <461147874@qq.com>
2018-04-09 00:04:58 +0800

    feat: 事件分发器

---
+++ components/area-cascader/emit.js
@@ -0,0 +1,36 @@
+class Emitter {
+    constructor () {
+        this.eventsMap = Object.create(null);
+    }
+
+    on (eventName, cb) {
+        if (typeof cb !== 'function') {
+            return;
+        }
+        const callbacks = this.eventsMap[eventName] || [];
+        callbacks.push(cb);
+        this.eventsMap[eventName] = callbacks;
+    }
+
+    emit (eventName, ...args) {
+        const callbacks = this.eventsMap[eventName];
+        callbacks && callbacks.forEach(cb => {
+            cb(...args);
+        });
+    }
+
+    off (eventName, cb) {
+        if (!cb) {
+            delete this.eventsMap[eventName];
+        } else if (typeof cb === 'function' && this.eventsMap[eventName]) {
+            const callbacks = this.eventsMap[eventName];
+            const index = callbacks.findIndex(callback => callback === cb);
+            if (index > -1) {
+                callbacks.splice(index, 1);
+                this.eventsMap[eventName] = callbacks;
+            }
+        }
+    }
+}
+
+export default new Emitter();
