f7ab76f56ee378fc23d67502c6f984ac21e668e7
dwqs <461147874@qq.com>
2018-04-09 00:38:41 +0800

    refactor: 删除旧组件

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './index.less';
 
-// import AreaSelect from './area-select';
-// import AreaCascader from './area-cascader';
 import AreaSelect from '../components/area-select/index';
 import AreaCascader from '../components/area-cascader/index';
 
