464ea4438c866e288d43c4a60fdd9367773b3811
dwqs <461147874@qq.com>
2018-04-09 00:36:30 +0800

    refactor: 更新 demo

--- gh/components/main/cas-basic.js
+++ gh/components/main/cas-basic.js
@@ -24,37 +24,19 @@ export default class CasBasic extends Component {
     }
 
     render () {
-        const { placeholder, level, size, type } = this.props;
 
         return (
             <div className="code-area">
                 <div className="area-left">
-                    <AreaCascader 
-                        level={level ? level : 0} 
-                        size={size ? size : ''}
-                        type={type ? type : ''}
-                        placeholder={placeholder ? placeholder : undefined} 
-                        onChange={this.handleSelectedChange}/>
+                    <AreaCascader onChange={this.handleSelectedChange}/>
                 </div>
                 <div className="area-right">
                     <pre><code>{`[${this.state.selected}]`}</code></pre>
                 </div>
                 {
-                    this.state.shown && !placeholder && !level &&
+                    this.state.shown && 
                     <div className="original-code">
                         <pre><code><span>//返回值是一个数组，分别是省市的行政区域代码</span><br /><span>&lt;</span>AreaCascader&nbsp;onChange=&#123;this.handleSelectedChange&#125;/<span>&gt;</span></code></pre>
-                    </div>
-                }
-                {
-                    this.state.shown && placeholder && !level &&
-                    <div className="original-code">
-                        <pre><code><span>//返回值是一个数组，分别是省市的行政区域代码</span><br /><span>&lt;</span>AreaCascader&nbsp;placeholder=&#123;'选择区域'&#125;&nbsp;onChange=&#123;this.handleSelectedChange&#125;/<span>&gt;</span></code></pre>
-                    </div>
-                }
-                {
-                    this.state.shown && level &&
-                    <div className="original-code">
-                        <pre><code><span>//返回值是一个数组，分别是省市的行政区域文本</span><br /><span>&lt;</span>AreaCascader&nbsp;type='text';&nbsp;level=&#123;1&#125;&nbsp;size='large'&nbsp;onChange=&#123;this.handleSelectedChange&#125;/<span>&gt;</span></code></pre>
                     </div>
                 }
                 <div className="show-code" onClick={this.toggle}>
--- gh/components/main/index.js
+++ gh/components/main/index.js
@@ -11,6 +11,7 @@ import DefaultVal from './default-value';
 import CasBasic from './cas-basic';
 import CasAreaTextCode from './cas-text-code';
 import CasAreaDef from './cas-def-value';
+import CasLink from './cas-linkage';
 
 export default class Main extends Component {
     constructor (){
@@ -40,33 +41,20 @@ export default class Main extends Component {
                 <p className='desc'>
                     <code>selected</code>&nbsp;是一个数组, 数组项顺序分别对应省/市/区/街道, 且类型(区域代码/区域文本)必须统一. 以第一个元素类型为基准. 类型不统一将报错.
                 </p>
-                <p className='desc'>
-                    <code>level</code>&nbsp;表示联动层级, 0-->一联 1->二联 2->三联 3->四联.
-                </p>
-                <h5>省市区街联动</h5>
-                <DefaultVal level={3} type='text' />
                 <h5>只选省份</h5>
                 <DefaultVal level={0} type='all' />
                 <h4>2. 作为级联器(只支持2/3级联动)</h4>
                 <h5>默认形式</h5>
                 <CasBasic />
-                <h5>设置 placeholder</h5>
-                <CasBasic placeholder='选择区域' />
-                <h5>设置默认值</h5>
-                <CasAreaDef />
                 <h5>返回区域文本和代码</h5>
                 <CasAreaTextCode />
                 <p className='desc'>
                     <code>type</code>&nbsp;表示返回值, 支持：['text', 'code', 'all'], 默认值是 code
                 </p>
+                <h5>设置默认值</h5>
+                <CasAreaDef />
                 <h5>省市区联动</h5>
-                <CasBasic level={1} size='large' type='text' />
-                <p className='desc'>
-                    <code>size</code>&nbsp;表示选择器大小, 支持：['large', 'default', 'small'], 默认值是 default
-                </p>
-                <p className='desc'>
-                    <code>level</code>&nbsp;表示联动层级, 0->二联 1->三联, 默认是0
-                </p>
+                <CasLink />
             </div>
         );
     }
